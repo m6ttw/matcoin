ff119f0404039d19ad5589faea99e166b8aeb38a
m6ttw <matthewrhyswilliams@live.com>
2020-12-27 16:57:52 +0000

    empty blockchain class created

--- blockchain.py
+++ blockchain.py
@@ -5,3 +5,19 @@ class Block():
     hash = None
     nonce = 0
     previous_hash = "0" * 64
+
+    def __init__(self, data, number=0):
+        self.data = data
+        self.number = number
+
+
+class Blockchain():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
